bfea5636bd862e3c40dc114714613039174c36ca
Edward Castillo <castilloedwarda@gmail.com>
2019-08-24 17:53:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board()
+def display_board(board)
   put " x  |  x "
   put "---------"
   put " x  |  x "
   put "---------"
   put " x  |  x "
 end
-  
+
+display_board()
