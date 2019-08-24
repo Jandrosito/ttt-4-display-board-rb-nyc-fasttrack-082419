6d01cac4ac4c76cf82d16c8168087bcb1c8316a9
Edward Castillo <castilloedwarda@gmail.com>
2019-08-24 18:02:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_board(board)
   put "------------"
   put " #{board[3]} | #{board[4]} | #{board[5]} "
   put "------------"
-  put " #{board[6]} | #{board[7] |  #{board[8]} "
+  put " #{board[6]} | #{board[7]} |  #{board[8]} "
 end
 
 display_board(board)
