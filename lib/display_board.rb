772998a918baa9addbb666b53cbda083ee4f7e31
Edward Castillo <castilloedwarda@gmail.com>
2019-08-24 18:01:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   put " #{board[0]} | #{board[1]} | #{board[2]}  "
-  put "---------"
+  put "------------"
   put " #{board[3]} | #{board[4]} | #{board[5]} "
-  put "---------"
-  put "   |   "
+  put "------------"
+  put " #{board[6]} | #{board[7] |  #{board[8]} "
 end
 
 display_board(board)
